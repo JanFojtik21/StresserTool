98618c0ecf4c00fe6b854b02470d983130d9251e
Durotan <50570415+JanFojtik21@users.noreply.github.com>
2023-12-05 12:50:31 +0100

    Add files via upload

---
+++ StresserTool/src/Main.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+
+
+public class Main{
+
+public static void main(String [] args){
+    System.out.println("Welcome back my friend ");
+
+    for (; cnt_send < req_cnt; cnt_sent++){
+        out.write()
+    }
+
+
+}
+
+
+
+}
